06baf875e8e2e72736608bb1039a34f7e997c6ab
SunshineVenus <externship.teamSunshineVenus@outlook.com>
2022-06-30 16:13:12 +0530

    changes for japan

--- helloworld.c
+++ helloworld.c
@@ -4,5 +4,7 @@ int main() {
    printf("Hello, World!");
     // printf() displays the string inside quotation
    printf("Hello, Aus
+// printf() displays the string inside quotation
+   printf("Hello, japan");
    return 0;
 
